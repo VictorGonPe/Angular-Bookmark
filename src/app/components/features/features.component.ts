74f2dcada2ec5be1ed0ac57918210c7188bca0e6
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-04-24 23:17:06 +0200

    wip: adding selector in the section feature

--- src/app/components/features/features.component.ts
+++ src/app/components/features/features.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-features',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './features.component.html',
   styleUrl: './features.component.scss'
 })
 export class FeaturesComponent {
+
+  //featuresSelector : {nameSelect: string, urlImage:}
 
 }
