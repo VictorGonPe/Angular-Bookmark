dc1b6cd720a625710fab4b81b22a62ab62a57032
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-05-02 13:21:11 +0200

    create the cards from downloads section

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  standalone: true,
+  imports: [ButtonComponent, CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+  card: { [key: string]: { imageCard: string; p1: string; p2: string; } } = {
+    chrome: {
+      imageCard: "assets/img/logo-chrome.svg",
+      p1: "Add to Chrome",
+      p2: "Minimum version 62"
+    },
+    firefox: {
+      imageCard: "assets/img/logo-firefox.svg",
+      p1: "Add to Firefox",
+      p2: "Minimum version 55"
+    },
+    opera: {
+      imageCard: "assets/img/logo-opera.svg",
+      p1: "Add to Opera",
+      p2: "Minimum version 46"
+    }
+  };
 
+  cardNames = Object.keys(this.card);
 }
--- src/app/components/download-extension/download-extension.component.ts
+++ src/app/components/download-extension/download-extension.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-download-extension',
+  standalone: true,
   imports: [],
   templateUrl: './download-extension.component.html',
   styleUrl: './download-extension.component.scss'
