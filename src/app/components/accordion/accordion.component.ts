c6500bd5e345d7da5bd03ebc8ec32c9311de6559
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-05-05 20:56:42 +0200

    feat: Create and add accordion components

--- src/app/components/accordion/accordion.component.ts
+++ src/app/components/accordion/accordion.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
+  standalone: true,
   imports: [],
   templateUrl: './accordion.component.html',
   styleUrl: './accordion.component.scss'
 })
 export class AccordionComponent {
-
+  textP = 'What is Bookmark?';
+  //@Input() isFirst:boolean = false;
 }
--- src/app/components/f-a-question/f-a-question.component.ts
+++ src/app/components/f-a-question/f-a-question.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AccordionComponent } from '../accordion/accordion.component';
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-f-a-question',
-  imports: [],
+  standalone: true,
+  imports: [AccordionComponent, ButtonComponent],
   templateUrl: './f-a-question.component.html',
   styleUrl: './f-a-question.component.scss'
 })
