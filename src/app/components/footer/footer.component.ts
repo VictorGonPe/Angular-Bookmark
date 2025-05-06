2e0d4b8275ec1d24b237ea8a70f84e9fff8201cd
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-05-06 18:18:35 +0200

    feat: Create sub-footer without social network

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../button/button.component";
+import { MenuLogoComponent } from "../menu-logo/menu-logo.component";
+import { SocialComponent } from "../social/social.component";
 
 @Component({
   selector: 'app-footer',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, MenuLogoComponent, SocialComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
