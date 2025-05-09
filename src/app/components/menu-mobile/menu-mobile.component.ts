c496a0587009eec2717741df3c7d2bcf32fff2aa
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-05-09 13:15:16 +0200

    feat: add botton and  styles in burger menu

--- src/app/components/menu-mobile/menu-mobile.component.ts
+++ src/app/components/menu-mobile/menu-mobile.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { MenuLogoComponent } from "../menu-logo/menu-logo.component";
+import { SocialComponent } from "../social/social.component";
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-menu-mobile',
   standalone: true,
-  imports: [MenuLogoComponent],
+  imports: [MenuLogoComponent, SocialComponent, ButtonComponent],
   templateUrl: './menu-mobile.component.html',
   styleUrl: './menu-mobile.component.scss'
 })
